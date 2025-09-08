4967ec5e9053a02a872b1c70be5bc2679b8fd0ce
Ritik Bhatt <bhritik2005@gmail.com>
2025-09-08 22:06:06 +0530

    changes

--- script.js
+++ script.js
@@ -35,3 +35,44 @@ if (window.innerWidth >= 768) {
     navItems.classList.add("hidden");
   });
 }
+
+// hero-slider
+
+ const slides = document.querySelectorAll('.slide');
+  let currentIndex = 0;
+  let interval = setInterval(nextSlide, 2000);
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.style.opacity = i === index ? "1" : "0";
+    });
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+  }
+
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+  }
+
+  document.getElementById("next").addEventListener("click", () => {
+    nextSlide();
+    resetInterval();
+  });
+  document.getElementById("prev").addEventListener("click", () => {
+    prevSlide();
+    resetInterval();
+  });
+
+  function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(nextSlide, 2000);
+  }
+
+  showSlide(currentIndex);
+
+// --------------------------------------------------
+
